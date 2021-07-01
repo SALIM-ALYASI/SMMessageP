3a0b830bc61d48e165a2433f9cdf8adf338524c2
الياسي والأمل <r.m.alyasi@gmail.com>
2021-07-01 22:20:05 +0400

    SMMessageP

---
+++ README.md
@@ -0,0 +1,3 @@
+# SMMessageP
+
+A description of this package.
---
+++ Tests/SMMessagePTests/SMMessagePTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import SMMessageP
+
+    final class SMMessagePTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(SMMessageP().text, "Hello, World!")
+        }
+    }

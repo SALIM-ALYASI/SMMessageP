a2720d69576fb1a27e480e2aaab579f608bf0eda
الياسي والأمل <r.m.alyasi@gmail.com>
2021-07-03 21:01:54 +0400

    activityIndicatorView

--- Sources/SMMessageP/SMMessageP.swift
+++ Sources/SMMessageP/SMMessageP.swift
@@ -1,5 +1,5 @@
 import UIKit
-let activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+ let activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
 var container: UIView = UIView()
 var loadingView: UIView = UIView()
  public extension UIViewController {
@@ -35,7 +35,38 @@ var loadingView: UIView = UIView()
 
       //  container.addSubview(activityIndicator)
     }
+    func showActivityIndicatora(_ title:String) {
+        let activityIndicatorView: ActivityIndicatorView = ActivityIndicatorView()
+        let Label : UILabel = UILabel()
+        container.frame = view.frame
+        container.center = view.center
+        container.backgroundColor = #colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 0.5089798479)
+        Label.text = title
+        loadingView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        loadingView.center = view.center
+        loadingView.backgroundColor = #colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 1)
+        loadingView.clipsToBounds = true
+        loadingView.layer.cornerRadius = 10
+        activityIndicatorView.frame = CGRect(x: 0, y: 0, width: 30.0, height: 30.0)
+      //  activityIndicator.style = UIActivityIndicatorView.Style.whiteLarge
+        activityIndicatorView.center = CGPoint(x: loadingView.frame.size.width / 2, y: loadingView.frame.size.height / 2);
+         Label.textAlignment = .center
+        Label.frame = CGRect(x: 100, y:100, width: 100.0, height: 50)
+         Label.center = CGPoint(x:loadingView.frame.size.width / 2, y: 80);
+         loadingView.addSubview(Label)
+         loadingView.addSubview(activityIndicatorView)
+        container.addSubview(loadingView)
+        view.addSubview(container)
+       // activityIndicator.startAnimating()
+        for subview in container.subviews {
+            if subview.tag == 100 {
+                print("already added")
+                return
+            }
+        }
 
+      //  container.addSubview(activityIndicator)
+    }
     func hideActivityIndicator() {
         let activityIndicator = container.viewWithTag(100) as? UIActivityIndicatorView
         activityIndicator?.stopAnimating()
---
+++ Sources/SMMessageP/untitled folder/ProgressView.swift
@@ -0,0 +1,67 @@
+//
+//  File.swift
+//  
+//
+//  Created by الياسي on 03/07/2021.
+//
+
+import UIKit
+
+@IBDesignable
+class ProgressView: UIView {
+    
+    @IBInspectable var trackColor: UIColor = .lightGray { didSet { setNeedsDisplay() } }
+    @IBInspectable var progressColor: UIColor = .darkGray { didSet { setNeedsDisplay() } }
+    @IBInspectable var lineWidth: CGFloat = 10 { didSet { setNeedsDisplay() } }
+    
+    /// Gets called each time the current progress change animation finishes
+    var animationDidFinish: (() -> Void)?
+    
+    /// Should be set to a value between 0.0 and 100.0
+    var progress = 0.0 { didSet { updateProgress(from: oldValue, to: progress) } }
+    
+    private var progressLayer = CAShapeLayer()
+    private var trackConfiguration: PathConfiguration {
+        return PathConfiguration(color: trackColor, lineWidth: lineWidth, startAngle: -CGFloat.pi / 2, type: .track)
+    }
+    private var progressConfiguration: PathConfiguration {
+        return PathConfiguration(color: progressColor, lineWidth: lineWidth, startAngle: -CGFloat.pi / 2, type: .progress)
+    }
+}
+
+// MARK: - View Drawing
+extension ProgressView {
+    
+    override func draw(_ rect: CGRect) {
+        
+        let trackLayer = CAShapeLayer()
+        trackLayer.drawCircle(in: rect, with: trackConfiguration)
+        self.layer.addSublayer(trackLayer)
+        
+        progressLayer.drawCircle(in: rect, with: progressConfiguration)
+        self.layer.addSublayer(progressLayer)
+    }
+    
+    private func updateProgress(from oldValue: Double, to newValue: Double, duration: CFTimeInterval = 0.2) {
+        
+        let animation = CABasicAnimation(keyPath: StrokePosition.strokeEnd.rawValue)
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        animation.fromValue = oldValue < 0 ? 0 : oldValue / 100
+        animation.toValue = newValue > 100 ? 100 : newValue / 100
+        animation.duration = duration
+        animation.fillMode = CAMediaTimingFillMode.forwards
+        animation.isRemovedOnCompletion = false
+        animation.delegate = self
+        
+        progressLayer.removeAnimation(forKey: StrokePosition.strokeEnd.rawValue)
+        progressLayer.add(animation, forKey: StrokePosition.strokeEnd.rawValue)
+    }
+    
+}
+
+extension ProgressView: CAAnimationDelegate {
+    
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        animationDidFinish?()
+    }
+}
